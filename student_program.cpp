5df79429a07c03e394e2f47362123f69ba293e89
Rxnai23 <129729905+Rxnai23@users.noreply.github.com>
2025-03-08 21:37:57 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[]) ;
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
+    char text[ 50 ] ;
+    char *output ;
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
+    
 }//end function
+
+char* reverse( char str1[] ) {
+    static char str2[99];   
+    static char reverse[99]; 
+
+    printf("Enter str: ");
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = '\0';
+
+    int length = strlen(str2);
+
+    for (int i = 0; i < length; i++) {
+        reverse[i] = str2[length - 1 - i];
+    }
+    reverse[length] = '\0'; 
+
+    printf( "FUNC : %s\n", reverse ) ;
+    return reverse;
+}
